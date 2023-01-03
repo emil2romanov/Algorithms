e3a24b14013162622c3f01f3bb41a4cfef6cc7a4
emil2romanov <eromanov8@gmail.com>
2023-01-03 22:21:43 +0200

    Algorithms

---
+++ Recursion and Backtracking - Lab/02RecursiveDrawing.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class RecursiveDrawing {
+    public static void main(String[] args) {
+      
+        Scanner console = new Scanner(System.in);
+
+        int number = Integer.parseInt(console.nextLine());
+
+        draw(number, "*", "#");
+    }
+
+    private static void draw(int number, String signTop, String signBottom) {
+      
+        if(number == 0) return;
+
+        System.out.println(drawRow(number, signTop));
+
+        draw(number-1, signTop, signBottom);
+
+        System.out.println(drawRow(number, signBottom));
+    }
+
+    private static String drawRow(int number, String sign){
+        //return sign.repeat(number); - не минава в judge
+      
+      String row = "";
+
+        for(int i = 0; i < number; i++){
+            row += sign;
+        }
+
+        return row;
+    }
+}
