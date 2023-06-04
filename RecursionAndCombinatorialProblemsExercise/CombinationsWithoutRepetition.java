f56844016e7749fd3af3c86eba53ff7f9f20bc1a
emil2romanov <eromanov8@gmail.com>
2023-06-04 03:53:35 +0300

    exercise

---
+++ RecursionAndCombinatorialProblemsExercise/CombinationsWithoutRepetition.java
@@ -0,0 +1,9 @@
+package recursionAndCombinatorialAlgorithmsExercise;
+
+import java.util.Scanner;
+
+public class CombinationsWithoutRepetition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
